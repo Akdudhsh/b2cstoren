22a346fed89d35400153af5f4f3e475f8149fe01
Lrx <209714995@qq.com>
2023-12-18 13:12:40 +0800

    项目基本结构_网关、通用服务和feign模块的搭建

---
+++ store-gateway/src/main/java/org/example/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package org.example.gateway;/**
+ * @author 罗蓉鑫
+ * @version 1.0
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 罗蓉鑫
+ * @Date: 2023/12/17 22:21
+ * @Description:
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
